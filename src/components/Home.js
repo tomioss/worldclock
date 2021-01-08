bc12008a78c58303d6e9c4952a97b00ada3540b2
Tom <abaotom14@gmail.com>
2021-01-08 08:32:02 +0800

    add home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ class Home extends Component {
   render() {
     return (
       // Home
-      <div id="home" className="row">
+      <div id="home">
         <div></div>
         <div>
-            Hello, World!
+          Hello, World!
         </div>
         <div></div>
       </div>
