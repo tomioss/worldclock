e73b47f1d51de84052d4bca1b4b54daa9e235e9c
Tom <abaotom14@gmail.com>
2021-01-08 08:23:24 +0800

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Home from './components/Home';
 
 import './main.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      // Home
+      <div id="home" className="row">
+        <div></div>
+        <div>
+            Hello, World!
+        </div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Home;
