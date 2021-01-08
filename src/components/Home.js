75a870107f188ce4f125005de279b8dc4891dd21
Tom <abaotom14@gmail.com>
2021-01-08 08:58:12 +0800

    map timezones

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
+import { now, now_la, now_ks, now_nz } from './DayUtils';
+
 class Home extends Component {
+  state = {
+    timezonesNow: {
+      'Local': now(),
+      'Los Angeles': now_la(),
+      'Kansas': now_ks(),
+      'New Zealand': now_nz()
+    }
+  };
+
   render() {
     return (
       // Home
       <div id="home">
         <div></div>
         <div>
-          Hello, World!
+          World Clock:
+          <ul>
+            {Object.entries(this.state.timezonesNow).map(([key, value]) =>
+              <li key={key}>
+                {key}: {value}
+              </li>
+            )}
+          </ul>
         </div>
         <div></div>
       </div>
