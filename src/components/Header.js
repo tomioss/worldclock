1780515f4f5b2e7602f125ebacaa7caf1c15c7ec
Tom <abaotom14@gmail.com>
2021-01-08 08:19:01 +0800

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import { menuItems } from "../constants/header";
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      active: menuItems[0].name
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(name) { 
+    this.setState({ active: name });
+  }
+
+  render() {
+    return (
+      // Navigation
+      <div id="header" className="row">
+        <ul className="taskbar">
+          {menuItems.map((menuItem, i) =>
+            <li
+              key={i}
+              className={this.state.active === menuItem.name ? 'active' : ''} 
+              onClick={this.handleClick.bind(this, menuItem.name)}
+            >
+              <a href={menuItem.link}>
+                {menuItem.name}
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
