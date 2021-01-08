dfa5bf5307047222ac03fe4014a8ca5c05508643
Tom <abaotom14@gmail.com>
2021-01-08 08:53:56 +0800

    add NZ timezone

--- src/components/DayUtils.js
+++ src/components/DayUtils.js
@@ -6,9 +6,11 @@ var timezone = require('dayjs/plugin/timezone')
 dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 dayjs.extend(timezone);
+
 const DATE_FORMAT = 'L LT';
 const TZ_LA = 'America/Los_Angeles';
 const TZ_KS = 'Etc/GMT+6';
+const TZ_NZ = 'Pacific/Auckland';
 
 export function now() {
   return dayjs().format(DATE_FORMAT);
@@ -20,4 +22,8 @@ export function now_la() {
 
 export function now_ks() {
   return dayjs().tz(TZ_KS).format(DATE_FORMAT);
+}
+
+export function now_nz() {
+  return dayjs().tz(TZ_NZ).format(DATE_FORMAT);
 }
