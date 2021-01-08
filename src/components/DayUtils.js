003085b45cdf8c4029ef5b3d994e6c750ee53363
Tom <abaotom14@gmail.com>
2021-01-08 08:49:57 +0800

    add LA timezone

--- src/components/DayUtils.js
+++ src/components/DayUtils.js
@@ -1,9 +1,18 @@
 var dayjs = require('dayjs');
 var localizedFormat = require('dayjs/plugin/localizedFormat');
+var utc = require('dayjs/plugin/utc');
+var timezone = require('dayjs/plugin/timezone')
 
 dayjs.extend(localizedFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 const DATE_FORMAT = 'L LT';
+const TZ_LA = 'America/Los_Angeles';
 
 export function now() {
   return dayjs().format(DATE_FORMAT);
+}
+
+export function now_la() {
+  return dayjs().tz(TZ_LA).format(DATE_FORMAT);
 }
