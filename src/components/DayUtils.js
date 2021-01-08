5bfb96c6aac84ccc2b207e3c12175319f2508d69
Tom <abaotom14@gmail.com>
2021-01-08 08:46:31 +0800

    use localizedFormat

--- src/components/DayUtils.js
+++ src/components/DayUtils.js
@@ -1,6 +1,8 @@
 var dayjs = require('dayjs');
+var localizedFormat = require('dayjs/plugin/localizedFormat');
 
-const DATE_FORMAT = 'MM/DD/YYYY hh:mm:ss A';
+dayjs.extend(localizedFormat);
+const DATE_FORMAT = 'L LT';
 
 export function now() {
   return dayjs().format(DATE_FORMAT);
